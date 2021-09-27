63ee1df79e29590c91919cd1ebf71bd6449eb674
tcsi800 <tcsi800@aucklanduni.ac.nz>
2021-09-27 19:23:52 +1300

    added data and started file to read in and analyse it

---
+++ data_analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+demand = pd.read_csv("WoolworthsDemands.csv")
+locations = pd.read_csv("WoolworthsLocations.csv")
+TravelDur = pd.read_csv("WoolworthsTravelDurations.csv")
