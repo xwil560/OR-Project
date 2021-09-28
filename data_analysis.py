3694ee620550917fd74c479847d25785b8da7d27
tcsi800 <tcsi800@aucklanduni.ac.nz>
2021-09-28 15:19:09 +1300

    started a Jupyter Notebook with some basic data analysis of the demands and created histograms

--- data_analysis.py
+++ data_analysis.py
@@ -3,4 +3,12 @@ import numpy as np
 
 demand = pd.read_csv("WoolworthsDemands.csv")
 locations = pd.read_csv("WoolworthsLocations.csv")
-TravelDur = pd.read_csv("WoolworthsTravelDurations.csv")
+travelDur = pd.read_csv("WoolworthsTravelDurations.csv")
+
+demand_long = demand.melt(id_vars = 'Store',var_name='Date', value_name='Demand' )
+
+demand_long.Date = pd.to_datetime(demand_long.Date)
+
+ts = pd.Series(demand_long[demand_long.Store=="FreshChoice Otahuhu"].Demand.values, index = demand_long[demand_long.Store=="FreshChoice Otahuhu"].Date)
+
+ts.plot()
