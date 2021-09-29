affb2f688825b14ba26ee3945378e08df0e848fa
John Cole <john@johncole.nz>
2021-09-29 18:13:22 +1300

    Added a weekend map and appropriate functions

--- maps/main.py
+++ maps/main.py
@@ -3,35 +3,109 @@ import pandas as pd
 import folium
 import json
 
-keys = []
-with open("keys.json") as fp:
-    keys = json.loads(fp.read())
 
-locations = pd.read_csv("../WoolworthsLocations.csv")
-coords = locations[['Long','Lat']]
-coords = coords.to_numpy().tolist()
+def create_map():
+    locations = pd.read_csv("../WoolworthsLocations.csv")
+    coords = locations[['Long','Lat']]
+    coords = coords.to_numpy().tolist()
 
-m = folium.Map(location = list(reversed(coords[2])), tiles=None, zoom_start=10)
+    m = folium.Map(location = list(reversed(coords[2])), tiles=None, zoom_start=10)
 
-tile_layer = folium.TileLayer(
-    tiles="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png",
-    attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    max_zoom=19,
-    name='darkmatter',
-    control=False,
-    opacity=0.7
-)
-tile_layer.add_to(m)
+    tile_layer = folium.TileLayer(
+        tiles="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png",
+        attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        max_zoom=19,
+        name='darkmatter',
+        control=False,
+        opacity=0.7
+    )
+    tile_layer.add_to(m)
 
-colour_dict = {
-    'Countdown':'green',
-    'FreshChoice':'blue',
-    'SuperValue':'red',
-    'Countdown Metro':'orange',
-    'Distribution Centre':'black'
+    colour_dict = {
+        'Countdown':'green',
+        'FreshChoice':'blue',
+        'SuperValue':'red',
+        'Countdown Metro':'orange',
+        'Distribution Centre':'black'
     }
 
-for i in range(0, len(coords)):
-    folium.Circle(list(reversed(coords[i])), radius=100, fill=True, popup= locations.Store[i], color=colour_dict[locations.Type[i]]).add_to(m)
+    for i in range(0, len(coords)):
+        folium.Circle(list(reversed(coords[i])), radius=250, fill=True, popup= locations.Store[i], color=colour_dict[locations.Type[i]]).add_to(m)
 
-m.save("index.html")
+    m.save("map.html")
+
+def create_weekend_map():
+    locations = pd.read_csv("../WoolworthsLocations.csv")
+    coords = locations[['Long','Lat']]
+    coords = coords.to_numpy().tolist()
+
+    m = folium.Map(location = list(reversed(coords[2])), tiles=None, zoom_start=10)
+
+    tile_layer = folium.TileLayer(
+        tiles="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png",
+        attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        max_zoom=19,
+        name='darkmatter',
+        control=False,
+        opacity=0.7
+    )
+    tile_layer.add_to(m)
+
+    colour_dict = {
+        'Countdown':'green',
+        'FreshChoice':'blue',
+        'SuperValue':'red',
+        'Countdown Metro':'orange',
+        'Distribution Centre':'black'
+    }
+
+    for i in range(0, len(coords)):
+        if locations.Type[i] == "Countdown":
+            folium.Circle(list(reversed(coords[i])), radius=250, fill=True, popup=locations.Store[i], color=colour_dict[locations.Type[i]]).add_to(m)
+
+    m.save("weekend_map.html")
+
+def generate_partitions():
+    locations = pd.read_csv("../WoolworthsLocations.csv")
+    coords = locations[['Long','Lat']]
+    coords = coords.to_numpy().tolist()
+
+    m = folium.Map(location = list(reversed(coords[2])), tiles=None, zoom_start=10)
+
+    tile_layer = folium.TileLayer(
+        tiles="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png",
+        attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        max_zoom=19,
+        name='darkmatter',
+        control=False,
+        opacity=0.7
+    )
+    tile_layer.add_to(m)
+
+    colour_dict = {
+        'Countdown':'green',
+        'FreshChoice':'blue',
+        'SuperValue':'red',
+        'Countdown Metro':'orange',
+        'Distribution Centre':'black'
+    }
+
+    demand_dict = {
+        'Countdown': 8,
+        'FreshChoice': 5,
+        'SuperValue': 5,
+        'Countdown Metro':5
+    }
+
+    for i in range(0, len(coords)):
+        folium.Circle(list(reversed(coords[i])), radius=250, fill=True, popup=locations.Store[i], color=colour_dict[locations.Type[i]]).add_to(m)
+
+    m.save("map.html")
+
+if __name__ == "__main__":
+    keys = []
+    with open("keys.json") as fp:
+        keys = json.loads(fp.read())
+
+    create_map()
+    create_weekend_map()
