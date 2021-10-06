bce88e3cf52890bb4c547048cbae8b1ffbee8183
John Cole <john@johncole.nz>
2021-10-07 00:09:09 +1300

    Changed and ran alternative solution

--- calculatecost.py
+++ calculatecost.py
@@ -4,20 +4,20 @@ import itertools as iter
 import json
 import os
 import pickle as pkl
-from tqdm import trange 
+from tqdm import trange
 
-def TSP_calculate(df_time,  stops):
+def TSP_calculate(df_time,  stops, weekend=False):
     '''
     Takes a list of stops and computes the best order to travel them in
-    
-    
+
+
     inputs:
     ------
     df_time : dataframe
         dataframe containing the time between each pair of locations
     stops : list
         list containing the stops that the route must go through
-    
+
     outputs:
     -------
     path : list
@@ -25,13 +25,30 @@ def TSP_calculate(df_time,  stops):
     cost : float
         cost (in nz$) of taking the trip
     '''
-    
+
     paths = list(iter.permutations(stops)) # create a list of all possible permutations
 
     df_time = df_time.set_index("Store")
 
+    locations = pd.read_csv("data/WoolworthsLocations.csv")
+
     df = df_time.loc[['Distribution Centre Auckland']+stops, ['Distribution Centre Auckland']+stops] # subset the dataframe so that only relevant stores are present
-    
+    df_locs = locations[locations['Store'].isin(list(stops))] # subset the dataframe so that only relevant stores are present
+
+    if weekend:
+        demand_dict = {
+            'Countdown': 4
+        }
+    else:
+        demand_dict = {
+            'Countdown': 8,
+            'FreshChoice': 5,
+            'SuperValue': 5,
+            'Countdown Metro':5
+        }
+
+    total_stops = sum([demand_dict[location] for location in df_locs['Type']])
+
     time = lambda p: path_time(df,p) # partial function evaluates the time of a path using the subsetted dataframe
 
     times = list(map(time, paths)) # generate list of times by mapping the time function to each possible path
@@ -40,35 +57,35 @@ def TSP_calculate(df_time,  stops):
 
     total_time = (times[mindex])/60 + 7.5*len(stops)
 
-    if total_time <= 240: # evaluate how expensive running this path is 
-        cost = 3.75*((times[mindex])/60 + 7.5*len(stops)) 
+    if total_time <= 240: # evaluate how expensive running this path is
+        cost = 3.75*((times[mindex])/60 + 7.5*total_stops)
     else:
-        cost = 3.75*240 + (275/60)*((times[mindex])/60 + 7.5*len(stops)-240)
+        cost = 3.75*240 + (275/60)*((times[mindex])/60 + 7.5*total_stops-240)
 
-    path = list(paths[mindex]) # convert the optimal path as a list 
-    
+    path = list(paths[mindex]) # convert the optimal path as a list
+
     return path, cost
 
 def path_time(df, path):
     '''
-    calculates the time taken to traverse a path in a certain order 
-    
-    
+    calculates the time taken to traverse a path in a certain order
+
+
     inputs:
     ------
     df : dataframe
         dataframe containing the time between each pair of locations
     path : list
         list showing the order in which the nodes are traversed
-    
+
     outputs:
     -------
     t : float
-        time taken to complete the trip in this order 
+        time taken to complete the trip in this order
     '''
 
     path = ["Distribution Centre Auckland"] + list(path) + ["Distribution Centre Auckland"] # make sure the path starts and ends in Favona
-    
+
     time = lambda l1,l2: df.loc[l1][l2] # time takes two locations and outputs the time taken to go between them
 
     t = sum([time(*p) for p in zip(path[:-1],path[1:])]) # calculate the time taken to traverse the path by summing the distances between each pair
@@ -82,13 +99,13 @@ def import_json(filename):
 def create_LP_values(filename):
     '''
     creates a dataframe to be used in the linear program formulation
-    
-    
+
+
     inputs:
     ------
     filename : string
-        name of the json file in the data folder containing hte relevant routes being used 
-    
+        name of the json file in the data folder containing hte relevant routes being used
+
     outputs:
     -------
     df : pandas Dataframe
@@ -96,27 +113,29 @@ def create_LP_values(filename):
     '''
 
     df_t = pd.read_csv("data" + os.sep + "WoolworthsTravelDurations.csv") # read in csv of times
-    
+
     df_t.rename({'Unnamed: 0':"Store"}, axis=1, inplace=True) # modify to the correct format for the TSP_calculate func
 
     routes = import_json("data" + os.sep + filename) # import subsets from json file
     leng = routes["total_combinations"]
-    
+
     dict = {
-        "path" : routes["combinations"], 
+        "path" : routes["combinations"],
         "cost" : [0]*leng
-        } # initialise dictionary to create 
-    
+        } # initialise dictionary to create
+
     for store in df_t.Store: # add a column for each location
         if store != "Distribution Centre Auckland":
             dict[store] = [0]*leng
-    
+
     df = pd.DataFrame(dict) # make into df
 
-
     for i in trange(leng): # for every route
-        route = df.iloc[i] # take data for route 
-        path, cost = TSP_calculate(df_t, route.path) # find the shortest path and its cost 
+        route = df.iloc[i] # take data for route
+        if "weekend" in filename:
+            path, cost = TSP_calculate(df_t, route.path, weekend=True) # find the shortest path and its cost
+        else:
+            path, cost = TSP_calculate(df_t, route.path, weekend=False) # find the shortest path and its cost
         df["path"].iloc[i] = path # add the optimal path and cost to the df
         df["cost"].iloc[i] = cost
         for loc in path: # for all stores the route goes through place 1 under all the columns corresponding to these stores
@@ -125,19 +144,19 @@ def create_LP_values(filename):
 
 
     return df
-    
+
 
 
 if __name__ == "__main__":
     # df = pd.read_csv("data" + os.sep + "WoolworthsTravelDurations.csv")
-    
+
     # df.rename({'Unnamed: 0':"Store"}, axis=1, inplace=True)
     # # df = df.set_index("Store")
-    
+
     # stops = ['Countdown Airport',  'Countdown Auckland City',  'Countdown Aviemore Drive']#,'Countdown Birkenhead','Countdown Blockhouse Bay']
     # p,c = TSP_calculate(df,  stops)
     # print(p,c)
     # print(create_LP_values())
     df = create_LP_values("combinations_weekend.json")
-    df.to_pickle("weekend_routes.pkl")
+    df.to_pickle("data/weekend_routes.pkl")
     # print(pd.read_pickle("weekday_routes.pkl"))
--- solve_lp.py
+++ solve_lp.py
@@ -9,13 +9,13 @@ def routes_solver(input_data_filename):
     '''
     Takes in a file name. Solves a linear problem, returning the cheapest price
     for pallet deliveries.
-    
-    
+
+
     inputs:
     ------
     input_data_filename : string
         file name of the routes, costs, locations being solved
-    
+
     outputs:
     -------
     list_of_routes : list
@@ -25,14 +25,14 @@ def routes_solver(input_data_filename):
 
     # Read in the pickle
     data =pd.read_pickle("data" + os.sep + input_data_filename)
-    
+
     # Organize the data into costs and Aki matrix
     cost = data["cost"]
     A = data.drop(["cost","path"],axis=1)
     A = A.loc[:,~A.eq(0).all()]
     routes = np.arange(len(cost)) # Array to keep truck of the amount of routes
 
-    # Generate binary variables for Amount of Trucks    
+    # Generate binary variables for Amount of Trucks
     R1=LpVariable.dicts("Shift 1",routes,0,1,LpBinary)
     R2=LpVariable.dicts("Shift 2",routes,0,1,LpBinary)
 
@@ -44,11 +44,11 @@ def routes_solver(input_data_filename):
 
     # Objective Function
     prob += lpSum([((DF2[i]+DF1[i])*2000) + (R1[i]+R2[i])*cost[i] for i in routes])
-    
+
     # Store Delivery Maximum
     for loc in A.columns:
         prob += lpSum([A[loc].iloc[i]*(R1[i]+R2[i]+DF1[i]+DF2[i]) for i in routes]) == 1, "Store Delivery Maximum"+loc
-    
+
     prob += lpSum([R1[i] for i in routes]) <= 30, 'Max trucks on Route 1'
     prob += lpSum([R2[i] for i in routes]) <= 30, 'Max trucks on Route 2'
 
@@ -70,14 +70,14 @@ def routes_solver(input_data_filename):
         if v.varValue == 1:
             print(data.path.iloc[int(v.name.split("_")[-1])], "=", v.varValue)
 
-    # The optimised objective function valof Ingredients pue is printed to the screen    
+    # The optimised objective function valof Ingredients pue is printed to the screen
     print("Total Cost from Routes = ", value(prob.objective))
 
     # Return a list of the stop numbers
-    list_of_routes = [int(v.name.split("_")[-1]) for v in prob.variables() if v.varValue == 1] 
+    list_of_routes = [int(v.name.split("_")[-1]) for v in prob.variables() if v.varValue == 1]
 
     return list_of_routes
 
 if __name__ == "__main__":
-    routes_solver("weekday_routes.pkl")
+    #routes_solver("weekday_routes.pkl")
     routes_solver("weekend_routes.pkl")
