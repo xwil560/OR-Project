50c970fa9df3715efb5be9a1d91b72dde0752bf8
John Cole <john@johncole.nz>
2021-10-19 23:28:16 +1300

    Minor bug fixes before submission

--- main.py
+++ main.py
@@ -33,11 +33,12 @@ if __name__ == "__main__":
     keys = {}
     keys['ORSkey'] = input("Please enter your ORS Key: ")
     
-    simulation_iterations = 10
+    simulation_iterations = 1000
     
     for routing_type in ["weekday_routesLOW", "weekend_routesLOW"]:
         print(f"Generating all project components for {routing_type}.")
         print("------------------------")
+        print("\n", end="")
         
         # Determine if it is a weekend
         weekend = True if "weekend" in routing_type else False
@@ -47,8 +48,8 @@ if __name__ == "__main__":
         # We start with "WoolworthsRegions.csv" which contains our stores
         # segmented into smaller regions based on geographical qualities.
 
-        print("Generating region partitions")
         # Generate the partitions from our regions.
+        print("Generating region partitions")
         if not weekend:
             partitions.generate_partitions(partition_data, weekend=False) 
 
@@ -58,22 +59,22 @@ if __name__ == "__main__":
 
             partitions.generate_partitions(partition_data, weekend=True) 
         
-        print("Generating routes and applying cost function")
         # Create routes and apply cost function
+        print("Generating routes and applying cost function")
         df = costs.create_LP_values(f"combinations_{week_stage}.json")
 
         df_rc = costs.route_cost(demands[routing_type], df)
 
         df_rc.to_pickle("differentDemands" + os.sep + f"{routing_type}.pkl")
 
-        print("Solving LP for routes to find inital optimal solution")
         # Solve LP for routes
+        print("Solving LP for routes to find inital optimal solution")
         routes, _, _ = lp.routes_solver(f"{routing_type}.pkl")
-        
+
         ors_client = ors.Client(key=keys['ORSkey'])
 
+        ## Plot solution on map
         print("Plotting solution on a map for visualisation")
-        # Plot solution on map
         locations = pd.read_csv("data" + os.sep + "WoolworthsLocations.csv", index_col='Store')
 
         # Map the resulting routes
@@ -81,23 +82,24 @@ if __name__ == "__main__":
         
         [line.add_to(m) for line in maps.generate_selected_routes(ors_client, routes, locations)]
 
-        m.save(f"maps/{routing_type}_map.html")
+        m.save(f"maps" + os.sep + "{routing_type}_map.html")
 
-        print("Simulating different demands for route selection")
         # Simulation
-        costs = sim.simulation(
+        print("Simulating different demands for route selection")
+        costs_sim = sim.simulation(
                             routes[:len(routes) // 2], 
                             routes[:(len(routes) - len(routes) // 2)], 
-                            weekend = False, 
+                            weekend = weekend, 
                             Nruns = simulation_iterations, 
                             filename = f"{routing_type}.pkl"
                             )
+        
+        save_filename = "WeekendLow.pkl" if weekend else "WeekendLow.pkl"
+        with open("cost_simulations" + os.sep + save_filename, "wb") as fp:
+            pkl.dump(costs_sim, fp)
 
-        with open("cost_simulations" + os.sep + f"{routing_type}.pkl", "wb") as fp:
-            pkl.dump(costs, fp)
-
-        print("Simulating the removal of a store")
         # Removed store simulation
+        print("Simulating the removal of a store")
         removed_store = "Countdown Northwest"
 
         routes_with_removed, _, _ = lp.extra_trucks_solver(
@@ -107,7 +109,7 @@ if __name__ == "__main__":
                                                     Ntrucks = 0
                                                     )
 
-        costs = sim.simulation(
+        costs_sim = sim.simulation(
                             routes_with_removed[:len(routes_with_removed) // 2], 
                             routes_with_removed[:(len(routes_with_removed) - len(routes_with_removed) // 2)], 
                             weekend = weekend, 
@@ -117,6 +119,6 @@ if __name__ == "__main__":
 
         save_filename = "NW_removed_wknd.pkl" if weekend else "NW_removed_wkdy.pkl"
         with open("cost_simulations" + os.sep + save_filename, "wb") as fp:
-            pkl.dump(costs,fp)
+            pkl.dump(costs_sim, fp)
 
         print(f"All done for {routing_type}") 
